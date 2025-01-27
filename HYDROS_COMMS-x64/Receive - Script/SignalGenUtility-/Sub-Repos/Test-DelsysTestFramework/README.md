bb2bec90310b4b18cf143780c27084f6cd2b487e
tylerbjoe <TJoe@delsys.com>
2025-01-27 13:00:50 -0500

    cleaning unnecessary files

--- HYDROS_COMMS-x64/Receive - Script/SignalGenUtility-/Sub-Repos/Test-DelsysTestFramework/README.md
+++
@@ -1,2 +0,0 @@
-# DelsysTestFramework
-Visa controls, a Lighter wrapper for the Delsys API ([auto] pairing), NI DAQ controls and more!
--- HYDROS_COMMS-x64/Transmit - Script 3 freq/FFTProj/FFTProj.h
+++
@@ -1,8 +0,0 @@
-#define FFTW_DLL
-#define FIND_DLL __declspec(dllexport) // import or export?
-#include <fftw3.h>
-#include <complex>
-
-using namespace std;
-extern "C" FIND_DLL int fftwFunc();
-extern "C" FIND_DLL void fftwTransform(std::complex<double>* input, std::complex<double>* output, int N);
