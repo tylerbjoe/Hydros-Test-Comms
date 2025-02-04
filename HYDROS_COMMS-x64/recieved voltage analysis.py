92ffa626e0ddca95c1104ee5cb03350f17bce34f
tylerbjoe <TJoe@delsys.com>
2025-02-04 12:52:30 -0500

    add python analysis file

---
+++ HYDROS_COMMS-x64/recieved voltage analysis.py
@@ -0,0 +1,126 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import struct
+from scipy.signal import stft, butter, filtfilt
+from scipy.signal import find_peaks
+from scipy.stats import linregress
+
+#%% LOAD DATA
+
+def load_signals_from_binary(file_path):
+    """
+    Load floating-point signals from a binary file.
+    Assumes the file contains consecutive float32 values.
+    
+    :param file_path: Path to the binary file
+    :return: List of float values
+    """
+    float_list = []
+    
+    with open(file_path, 'rb') as file:
+        while chunk := file.read(4):  # Read 4 bytes at a time (size of float32)
+            # Unpack the binary data to a float and append to the list
+            float_list.append(struct.unpack('f', chunk)[0])
+    
+    return float_list
+
+def get_fft(signal, fs):
+    # Create an FFTW object
+    fft_result = np.fft.fft(signal)
+    fft_vals = np.abs(fft_result) / len(signal)
+    
+    freqs = np.fft.fftfreq(len(signal), 1 / fs)
+    return freqs[:len(signal) // 2], fft_vals[:len(signal) // 2]
+
+def integrate_fft_power(freqs, power_spectrum, center_freq=100000, bandwidth=2500):
+    """
+    Integrate the power spectral density in a given frequency band.
+    
+    :param freqs: Frequency array from FFT
+    :param power_spectrum: Squared magnitude of FFT
+    :param center_freq: Center frequency for integration (default 100 kHz)
+    :param bandwidth: Half-bandwidth for integration (default ±2.5 kHz)
+    :return: Integrated power over the specified frequency range
+    """
+    lower_bound = center_freq - bandwidth
+    upper_bound = center_freq + bandwidth
+
+    # Find indices within the frequency range
+    indices = np.where((freqs >= lower_bound) & (freqs <= upper_bound))
+
+    # Integrate (sum) power in the frequency range
+    integrated_power = np.sum(power_spectrum[indices])
+    return integrated_power
+
+
+#%% Processing multiple packets
+fs = 1_000_000  # Sampling frequency in Hz
+index = [1,10,20]#np.arange(1, 21)  # Packet tx voltages
+packet=[]
+for ind in index:
+    file_path = fr"C:\Users\TJoe\OneDrive - Delsys Inc\TJ HYDROS\HYDROS comms\Recieved Demod Test 1\packet{ind}v.bin"
+    
+    # Load and normalize signal
+    out1 = load_signals_from_binary(file_path)
+    normalized_out1 = out1 - np.mean(out1)
+    
+    # Compute FFT
+    out1_freqs, out1_power_spectrum = get_fft(normalized_out1, fs)
+    
+    # Integrate power around 100 kHz ± 2.5 kHz
+    power_integral = integrate_fft_power(out1_freqs, out1_power_spectrum, center_freq=100000, bandwidth=2500)
+    
+    print(f"Packet {ind}: Integrated power in {100000} ± {2500} Hz = {power_integral}")
+    
+    packet.append(power_integral)
+
+#%% Plot FFT
+# Plot the frequency spectrum
+# plt.figure(figsize=(10, 6))
+# plt.plot(out1_freqs, out1_fftvals)
+# plt.title("10v packet 2v sine")
+# plt.xlabel("Frequency (Hz)")
+# plt.ylabel("Magnitude")
+# plt.grid()
+# plt.show()
+
+
+#%% Plot sft
+# Zxx = zxx[::2, ::2]
+# f = out1_sfreqs[::2]
+# t = out1_stimes[::2]
+
+# Zxx = zxx
+# f = out1_sfreqs
+# t = out1_stimes
+
+
+# plt.figure(figsize=(10, 6))
+# plt.pcolormesh(t, f, np.abs(Zxx), shading='gouraud')
+# plt.title("10v packet 2v sine")
+# plt.ylabel("Frequency [Hz]")
+# plt.xlabel("Time [s]")
+# plt.colorbar(label="Magnitude")
+# plt.tight_layout()
+# plt.show()
+
+# # #%%
+
+#%%
+#plot rx psd and linear model
+# Perform linear regression
+index = np.array(index)
+plt.figure()
+plt.title('Power Spectral Density at Test Position')
+
+plt.plot(index,packet,label='Packet')
+plt.plot(index,index*linregress(index,packet).slope,label=f'slope={linregress(index,packet).slope:.2f}', linestyle='--', color='grey')
+
+plt.xlabel('Tx Voltage')
+plt.ylabel('Rx Packet PSD (Voltage^2)')
+    
+plt.legend()
+plt.show()
+    
